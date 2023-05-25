4d084212c0daa762371b203480a03c86fa49dc9e
derique96 <fowlkes.derique@gmail.com>
2023-05-25 19:45:11 -0400

    menu and schedule main routes created

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,13 @@
 const router = require('express').Router();
 
-
-const employeeRoutes = require('./employee/employee-routes.js');
 const homeRoutes = require('./home-routes.js');
+const employeeRoutes = require('./employee/employee-routes.js');
+const menuRoutes = require('./menu/menu-routes.js');
+
 
 
 router.use('/', homeRoutes);
 router.use('/employee', employeeRoutes);
-
+router.use('/menu', menuRoutes);
 
 module.exports = router;
