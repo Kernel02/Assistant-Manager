40283864694c181a743e0e8729d828d2fa33a129
derique96 <fowlkes.derique@gmail.com>
2023-05-25 18:55:56 -0400

    employee folder made

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,12 @@
 const router = require('express').Router();
 
+
+const employeeRoutes = require('./employee');
+const homeRoutes = require('./home-routes.js');
+
+
+router.use('/', homeRoutes);
+router.use('/employee', employeeRoutes);
+
+
 module.exports = router;
