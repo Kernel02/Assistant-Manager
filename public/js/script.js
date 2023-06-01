1a47fe92e8d9a849c8d35d35be1e1c70afbc0c1f
Kernel02 <123792001+Kernel02@users.noreply.github.com>
2023-05-31 20:20:13 -0400

    changed fonts, dropdown arrow etc.

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,18 @@
+const arrowIcon = $("#arrowDown");
+
 $(document).ready(function () {
   $(".parallax").parallax();
   $(".dropdown-trigger").dropdown();
 });
-console.log("Hello");
+
+function changeArrow() {
+  if (arrowIcon[0].id === "arrowDown") {
+    $(arrowIcon[0]).text("arrow_drop_up");
+    $(arrowIcon[0]).attr("id", "arrowUp");
+  } else {
+    $(arrowIcon[0]).text("arrow_drop_down");
+    $(arrowIcon[0]).attr("id", "arrowDown");
+  }
+}
+
+$(".dropdown-trigger").on("click", changeArrow);
