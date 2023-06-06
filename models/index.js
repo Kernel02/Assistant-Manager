847f5b7a188f03b7797d233c44b83ae4a1627f59
Your Name <you@example.com>
2023-06-06 16:47:35 -0700

    updated empmenurole

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const Employee = require("./employee");
 const Menu = require("./menu");
 const Role = require("./Role");
+const User = require("./User");
 
 Employee.belongsTo(Role, {
   foreignKey: "role_id",
+ 
 });
 
-module.exports = { Employee, Menu, Role };
+module.exports = { Employee, Menu, Role, User };
