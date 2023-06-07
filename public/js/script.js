982dd1c898d6215eb0ada154e65c95e500cef8d2
Kernel02 <123792001+Kernel02@users.noreply.github.com>
2023-06-06 20:27:23 -0400

    js for forms and routes

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,77 @@
 const arrowIcon = $("#arrowDown");
+const employeeForm = $("#employee-form");
+const roleForm = $("#role-form");
+const menuForm = $("#menu-item-form");
+const dropdown = $(".dropdown-trigger");
+
+const employeeHandler = async (event) => {
+  event.preventDefault();
+
+  const first_name = event.target[0].value;
+  const last_name = event.target[1].value;
+  const role_id = event.target[3].value;
+  const phone = event.target[4].value;
+  const email = event.target[5].value;
+  const address = event.target[6].value;
+
+  const response = await fetch("/employee", {
+    method: "POST",
+    body: JSON.stringify({
+      first_name,
+      last_name,
+      phone,
+      email,
+      address,
+      role_id,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+const roleHandler = async (event) => {
+  event.preventDefault();
+  const title = event.target[0].value;
+  const description = event.target[1].value;
+  const salary = event.target[2].value;
+
+  console.log(title, description, salary);
+
+  const response = await fetch("/employee/role", {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      description,
+      salary,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
+
+const menuHandler = async (event) => {
+  event.preventDefault();
+
+  const name = event.target[0].value;
+  const description = event.target[1].value;
+  const price = event.target[2].value;
+  const calory = event.target[3].value;
+
+  const response = await fetch("/menu", {
+    method: "POST",
+    body: JSON.stringify({
+      name,
+      description,
+      price,
+      calory,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+};
 
 $(document).ready(function () {
   $(".parallax").parallax();
@@ -18,4 +91,7 @@ function changeArrow() {
   }
 }
 
-$(".dropdown-trigger").on("click", changeArrow);
+dropdown.on("click", changeArrow);
+employeeForm.on("submit", employeeHandler);
+roleForm.on("submit", roleHandler);
+menuForm.on("submit", menuHandler);
