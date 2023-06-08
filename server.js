28b640c743cf5b1422843dd4c569c2b481f70091
Kernel02 <123792001+Kernel02@users.noreply.github.com>
2023-06-07 20:13:17 -0400

    session

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const hbs = exphbs.create();
 const sessionOptions = {
   secret: "Super secret secret",
   cookie: {
-    maxAge: 300000,
+    maxAge: 60 * 60 * 1000,
     httpOnly: true,
     secure: false,
     sameSite: "strict",
